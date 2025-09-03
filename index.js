65f1ece32ec42bd96df5b3741369d0026fffd8e3
paujorbaa <pau.jorbaa@gmail.com>
2025-09-03 14:36:56 +0200

    done

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
-
+let tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4;
+console.log(tongueTwister)
 // Print out the concatenated string
 
 
@@ -22,12 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const camelTail1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const camelTail2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 // Print the cameLtaiL-formatted string
+const result = camelTail1 + camelTail2
 
-
-
+console.log(result)
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,11 +35,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+tipAmount = billTotal / 100 * 15
 
 // Print out the tipAmount
 
-
+console.log(tipAmount)
 
 
 /*******************************************
@@ -47,11 +47,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randNum = Math.floor(Math.random() * 10)
 
 // Print the generated random number
 
-
+console.log(randNum)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +62,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+// false
 const expression2 = a || b;
-
+// true
 const expression3 = !a && b;
-
+// false
 const expression4 = !(a && b);
-
+// true
 const expression5 = !a || !b;
-
+// true
 const expression6 = !(a || b);
-
+// false
 const expression7 = a && a;
+// true
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
